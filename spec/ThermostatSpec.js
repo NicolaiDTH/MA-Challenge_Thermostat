6dad32f1046edb0be550d4416913083ba3ad7f4b
Nicolai <nicolai@mmg.io>
2014-10-24 14:08:32 +0100

    Drying out variables in tests

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,21 +1,21 @@
 describe("Thermostat", function(){
  var thermostat;
 
- it("is initially at 20 degrees", function(){
+ beforeEach(function(){
+  thermostat = new Thermostat();
+ });
 
-  var thermostat = new Thermostat();
+ it("is initially at 20 degrees", function(){
   expect(thermostat.temperature).toEqual(20)
 
  })
 
  it("has PSM on by default", function(){
-  var thermostat = new Thermostat();
   expect(thermostat.powerSavingMode).toBe(true);
  })
 
  describe("Maximum Temperature", function(){
   it("is 25 degrees with PSM", function(){
-   var thermostat = new Thermostat();
    expect(thermostat.maxiumumTemperature).toEqual(25);
   })
  })
