a936bac238fffc1a2fc9b48938d9411302b139e9
Nicolai <nicolai@mmg.io>
2014-10-24 14:07:11 +0100

    Some tests passing

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,3 +1,23 @@
 describe("Thermostat", function(){
+ var thermostat;
+
+ it("is initially at 20 degrees", function(){
+
+  var thermostat = new Thermostat();
+  expect(thermostat.temperature).toEqual(20)
+
+ })
+
+ it("has PSM on by default", function(){
+  var thermostat = new Thermostat();
+  expect(thermostat.powerSavingMode).toBe(true);
+ })
+
+ describe("Maximum Temperature", function(){
+  it("is 25 degrees with PSM", function(){
+   var thermostat = new Thermostat();
+   expect(thermostat.maxiumumTemperature).toEqual(25);
+  })
+ })
 
 });
